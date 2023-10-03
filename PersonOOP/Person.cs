0001e676c1ca9346fd1c6cec575940d1a2b8a51a
TomikDavid <tomik.david1@gmail.com>
2023-10-03 10:54:45 +0200

    Add project files.

---
+++ PersonOOP/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonOOP
+{
+    internal abstract class Person
+    {
+        private string nev;
+        private int kor;
+        private string nem;
+
+        public Person(string nev, int kor, string nem)
+        {
+            this.nev = nev;
+            this.kor = kor;
+            this.nem = nem;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Kor { get => kor; set => kor = value; }
+        public string Nem{ get => nem; set => nem = value; }
+
+        public abstract string bemutatkozas() 
+        {
+            return $"Hi, I'm {this.nev}, a {this.kor} year old {this.nem}.";
+        }
+        public abstract string getGoal() 
+        { 
+            return "My goal is: Live for the moment!";
+        }
+    }
+}
+
---
+++ PersonOOP/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonOOP
+{
+    internal class Student : Person
+    {
+        private string iskola;
+        private int hilyányzotnapok;
+        public Student(string nev, int kor, string nem, string iskola) : base(nev, kor, nem)
+        {
+            this.iskola = iskola;
+            this.hilyányzotnapok = 0;
+        }
+        public override string getGoal()
+        {
+            return "Be a junior software developer.";
+        }
+        public override string bemutatkozas()
+        {
+            throw new NotImplementedException();
+        }
+        public void skipday()
+        {
+            hilyányzotnapok++;
+        }
+    }
+}
